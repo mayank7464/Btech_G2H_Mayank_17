0cc7302823d9ad74ef7c0592aa7d095e635b4cca
mayank7464 <25110cn552@glbitm.ac.in>
2026-02-13 12:05:06 +0530

    Add files via upload

---
+++ c program6.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+   int main()
+{  float a,b;
+    printf("enter two numbers \n");
+    scanf("%f%f",&a,&b);
+    if (a==b)
+    { printf("Two numbers are equal");
+    }  
+    else
+    { printf("Two numbers are not equal");
+    }
+    return 0;
+    }
