25e8122a91a3f088a177ae9797087435332ef269
mayank7464 <25110cn552@glbitm.ac.in>
2026-02-13 12:29:39 +0530

    Add files via upload

---
+++ c program 8.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+   int main()
+{  int a;
+    printf("enter number to check \n");
+    scanf("%d",&a);
+    if (a%2==0)
+    { printf(" given number is even");
+    }  
+    else
+    { printf(" given number is odd");
+    }
+    return 0;
+    }
